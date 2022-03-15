bbbabc8349021f27dc5f443bcff7780cff80cc44
人人都是孙笑川 <99889567+yze142@users.noreply.github.com>
2022-03-16 00:27:55 +0800

    Update UserMapper.java

--- lagou/ssm-dao/src/main/java/com/lagou/dao/UserMapper.java
+++ lagou/ssm-dao/src/main/java/com/lagou/dao/UserMapper.java
@@ -37,6 +37,8 @@ public interface UserMapper {
 
     //4.根据角色id获取资源信息
    public List<Resource>   findResourceByRoleId(List<Integer> ids);
+    
+     public List<Resource>   afindResourceByRoleId(List<Integer> ids);
 
 
 }
