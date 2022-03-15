db43f9e213583a481f05a5a6da28ef367b0ad216
人人都是孙笑川 <99889567+yze142@users.noreply.github.com>
2022-03-16 00:41:48 +0800

    Update UserMapper.java

--- lagou/ssm-dao/src/main/java/com/lagou/dao/UserMapper.java
+++ lagou/ssm-dao/src/main/java/com/lagou/dao/UserMapper.java
@@ -39,6 +39,8 @@ public interface UserMapper {
    public List<Resource>   findResourceByRoleId(List<Integer> ids);
     
     /* public List<Resource>   afindResourceByRoleId();*/
+    //sadfsdafsdaf
+    ///dsafdsaf
 
 
 }
